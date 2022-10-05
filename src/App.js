2e086281a9af8ea1c14e8f4c76bb2755becf5142
luan <luanvitors_852@hotmail.com>
2022-10-05 13:37:46 -0300

    criando base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Routes from "./Services/routers.js"
+import * as S from "./Global/GlobalStyle"
+
+export default function App() {
+
+  return (
+    <>
+      <S.GlobalStyle />
+      <Routes />
+    </>
+  )
+}
